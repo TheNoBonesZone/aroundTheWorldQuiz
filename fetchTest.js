d6f94c074c7e73314ac07749a7f2b63e2ee6c7d6
Danielle-91 <danielle.alamilla@gmail.com>
2021-11-06 13:11:16 -0400

    Changed js file name

--- fetchTest.js
+++ fetchTest.js
@@ -4,26 +4,16 @@ fetch(url)
   .then(res => {
     return res.json() // Promise
   }).then(resJson => {
-    const bigCountriesData = myShuffle(resJson); // country array
+    const bigCountriesData = shuffleArray(resJson); // country array
     // const bigCountriesData2 = myShuffle(bigCountriesData);// country array
     console.log(bigCountriesData); // before Shuffle
     // console.log(bigCountriesData2);
   })
 
-const myShuffle = (array) => {
-  let currentIndex = array.length, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-
-  return array;
 }
+
